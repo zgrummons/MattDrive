0428dc7e538d930a6f92172a5f314795883350e4
Zachariah Grummons <zgrummons@gmail.com>
2018-01-17 10:29:54 -0600

    backpedaling on getdirectory()

--- GoogleDrive1/Models/DirectoryModel.cs
+++ GoogleDrive1/Models/DirectoryModel.cs
@@ -11,13 +11,13 @@ namespace GoogleDrive1.Models
     public class DirectoryModel
     {
         public string Id { get; set; }
-        public IList<Google.Apis.Drive.v3.Data.File> Files { get; set; }
-        public Stack<string> Parents = new Stack<string>();
+        public IList<File> Files { get; set; }
+        public Stack<File> Parents = new Stack<File>();
 
         public static DirectoryModel Instance { get; } = new DirectoryModel();
 
         private DirectoryModel()
-        {
+        {            
         }
 
         public void GetDirectory(File file)
@@ -25,18 +25,18 @@ namespace GoogleDrive1.Models
             var filename = file.Id ?? "root";
 
             if (file.MimeType != "application/vnd.google-apps.folder" && file.Id != null) return;
-            if (Parents.Contains(file.Name))
-                while (Parents.Count > 0 && Parents.Peek() != file.Name)
-                    Parents.Pop();
-            else
-                Parents.Push(file.Name);
+            //if (Parents.Contains(file.Parents))
+            //    while (Parents.Count > 0 && Parents.Peek() != file.Name)
+            //        Parents.Pop();
+            //else
+            //    Parents.Push(filename);
 
             // Define parameters of request.
             FilesResource.ListRequest listRequest = DataAccess.DriveService.Files.List();
             listRequest.Q = $"\'{filename}\' in parents";
             listRequest.OrderBy = "folder, name asc";
             listRequest.PageSize = 1000;
-            listRequest.Fields = "nextPageToken, files(id, name)";
+            listRequest.Fields = "nextPageToken, files(id, name, parents)";
             // List files.
             Files = listRequest.Execute().Files;
         }
